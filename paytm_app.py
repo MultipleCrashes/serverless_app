15940c55bcb2121513facfbaeedc3d2aba1c8c7d
MultipleCrashes <MultipleCrashes@users.noreply.github.com>
2017-07-07 03:54:10 +0530

    Update paytm_app.py
    serverless

--- paytm_app.py
+++ paytm_app.py
@@ -3,6 +3,9 @@ from urllib.request import urlopen
 from pprint import pprint 
 import json 
 
+# serverless
+
+
 def main(dict):
     request_url = """https://pguat.paytm.com/oltp/HANDLER_INTERNAL/getTxnStatus?JsonData={%22MID%22:%22klbGlV59135347348753%22,%22ORDERID%22:%22ORDER48886809916%22,%22CHECKSUMHASH%22:%22wctrePBbNfJkGyNXRg8sHBTJZWGJEFMBuUtO3qoFkL2ox8HIe9YSzTU%2FpswpDbtAhS%2bkWiHgr5nmu9z9cn8rbzGsV0qy8D16c2negimoD%2Fs%3D%22}"""
     resp = urlopen(request_url)
